a858f9e89a0d88b5da86daabd05f9bd84b810279
Hello <lethanhphong2605@gmail.com>
2023-04-02 05:56:00 +0700

    add code

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from django.template import loader
+
+def exam_result(request):
+  template = loader.get_template('exam_result_bootstrap.html')
+  return HttpResponse(template.render())
+def course_detail(request):
+  template = loader.get_template('course_detail_bootstrap.html')
+  return HttpResponse(template.render())
