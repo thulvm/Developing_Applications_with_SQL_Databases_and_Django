0a135c3177bd54075b43aafbc240605669d9bc29
Hello <lethanhphong2605@gmail.com>
2023-04-02 05:45:40 +0700

    add code

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.db import models
+
+class Question(models.Model):
+  content = models.CharField(max_length=255)
+
+class Choice(models.Model):
+  content = models.CharField(max_length=255)
