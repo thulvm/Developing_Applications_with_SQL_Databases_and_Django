34e31f803ebb8a0176fbc37b7f4a7887381319eb
Hello <lethanhphong2605@gmail.com>
2023-04-02 07:49:42 +0700

    commit code

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
-from django.db import models
-
-class Question(models.Model):
-  content = models.CharField(max_length=255)
-
-class Choice(models.Model):
-  content = models.CharField(max_length=255)
+admin.site.register(Question)   #new
+admin.site.register(Choice)     #new
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Question(models.Model):
+  content = models.CharField(max_length=255)
+
+class Choice(models.Model):
+  content = models.CharField(max_length=255)
