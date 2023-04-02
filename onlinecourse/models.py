724bbf4255b6abc7af96529039f6d67fddf6ebed
Hello <lethanhphong2605@gmail.com>
2023-04-02 08:59:44 +0700

    commit code

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class Question(models.Model):
-  content = models.CharField(max_length=255)
+    question_text = models.CharField(max_length=200)
+    
+    def __str__(self):
+      return self.question_text
+
 
 class Choice(models.Model):
-  content = models.CharField(max_length=255)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    
+    def __str__(self):
+      return self.choice_text
